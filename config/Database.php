fafda53a29f5da2b9b285667ca6e5177ddda3216
Yogi Ghorecha <yogi.ghorecha@gmail.com>
2026-02-12 09:55:31 +0100

    Add Database class for database connection management

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,30 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "rest_api_db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
 
+    public function getConnection() {
+        $this->conn = null;
+        
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Connection error: ' . $exception->getMessage()
+            ]);
+        }
+        
+        return $this->conn;
+    }
+}
+?>
